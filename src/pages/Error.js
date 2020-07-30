46bceaae61bb646d70fdc6f40c73a98997ef354b
lawrence <lawrenceo@activedgetechnologies.com>
2020-07-31 00:28:25 +0100

    bootstrapped login

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Error = () => {
 	return (
 		<Wrapper>
-			<div>
+			<article>
 				<h1>404</h1>
 				<h3>Sorry, the page you tried cannot be found</h3>
 				<Link className='btn' to='/'>
 					back home
 				</Link>
-			</div>
+			</article>
 		</Wrapper>
 	);
 };
