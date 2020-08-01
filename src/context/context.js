2a86c9d4eeaa0f19bfefd2ae5e92f70a42c9a5e6
lawrence <lawrenceo@activedgetechnologies.com>
2020-08-01 10:57:45 +0100

    finished dynamic search and default loading

--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,37 @@ const GithubProvider = ({ children }) => {
 	const [ githubUser, setGithubUser ] = useState(mockUser);
 	const [ githubRepos, setGithubRepos ] = useState(mockRepos);
 	const [ githubFollowers, setGithubFollowers ] = useState(mockFollowers);
-	const githubProps = { githubUser, githubRepos, githubFollowers };
-	return <GithubContext.Provider value={{ ...githubProps }}>{children}</GithubContext.Provider>;
+	const [ requests, setRequests ] = useState(0);
+	const [ isLoading, setIsLoading ] = useState(true);
+	const [ error, setError ] = useState({ show: false, message: "" });
+	const githubProps = { githubUser, githubRepos, githubFollowers, requests, error, isLoading };
+
+	const searchGithubUser = async (user) => {
+		try {
+			setError({ show: false, message: "" });
+			const { data } = await axios.get(`${rootUrl}/users/${user}`);
+			console.log("data", data);
+			// setLoading
+			setGithubUser(data);
+		} catch (error) {
+			if (error.response.status === 404) setError({ show: true, message: "There is no user with that username" });
+			console.log("error", error.response);
+		}
+	};
+
+	const checkRquests = async () => {
+		try {
+			const { data: { rate: { remaining } } } = await axios.get(`${rootUrl}/rate_limit`);
+			setRequests(remaining);
+			if (remaining === 0) setError({ show: true, message: "Sorry you have exceeded your hourly limit!" });
+		} catch (error) {
+			console.log(error.response);
+		}
+	};
+	useEffect(() => {
+		checkRquests();
+	}, []);
+	return <GithubContext.Provider value={{ ...githubProps, searchGithubUser }}>{children}</GithubContext.Provider>;
 };
 
 export { GithubContext, GithubProvider };
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Info, Repos, User, Search, Navbar } from "../components";
 import loadingImage from "../images/preloader.gif";
 import { GithubContext } from "../context/context";
 const Dashboard = () => {
+	const { isLoading } = useContext(GithubContext);
+	if (isLoading) return <img src={loadingImage} className='loading-img' alt='loading' />;
 	return (
 		<main>
 			<Navbar />
